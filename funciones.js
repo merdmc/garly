0a3e69a32ebd451bd29443902e23a1688fb951a9
Matias Rodriguez <merdmc@gmail.com>
2023-01-25 12:52:39 -0300

    Primer escritura

---
+++ funciones.js
@@ -0,0 +1,20 @@
+document.getElementById('boton2').onclick = function capturar() {
+    console.log("Capturar click de mostrar fuente");
+
+}
+
+
+
+
+
+
+function mostrar(){
+    document.getElementById('container').style.display = 'inline-block';
+
+}
+
+function ocultar(){
+    document.getElementById('container').style.display = 'none';
+
+}
+
